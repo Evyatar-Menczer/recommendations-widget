a19c2c9435f0ce2b984dfde91c16023c02f92015
Evyatar-Menczer <evyatar.menczer@gmail.com>
2024-03-25 01:31:35 +0200

    http -> https

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,5 +4,5 @@ const secrets = getSecrets();
 export const config = {
     TABOOLA_API_KEY: secrets.TABOOLA_API_KEY,
     BASE_URL:
-        "http://api.taboola.com/1.0/json/taboola-templates/recommendations.get",
+        "https://api.taboola.com/1.0/json/taboola-templates/recommendations.get",
 };
